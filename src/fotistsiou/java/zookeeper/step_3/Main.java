998383cd72026ddeae22ffdb038a2a6a3f9407c7
fotistsiou <fotis.tsioumas@outlook.com>
2024-11-18 10:09:01 +0200

    Update project

---
+++ src/fotistsiou/java/zookeeper/step_3/Main.java
@@ -0,0 +1,23 @@
+package fotistsiou.java.zookeeper.step_3;
+
+/**
+ * Show where animals live
+ * -----------------------
+ * Description
+ * The third stage requires you to increase the capabilities of your software. Now it should be able to recognize the
+ * number of a specific habitat from the input and show the animals living there.
+ * Add all of the variables from the template to a single variable with the list type. The order of variables matters:
+ * they must appear on the list in the order in which they're defined in the code. The list must contain all of the
+ * variables with no duplicates.
+ * -----------------------
+ * Objectives
+ * In this stage your program should:
+ * 1. Ask for the number of the desired habitat using the following phrase: Please enter the number of the habitat you would like to view:
+ * 2. Use the input number as an index of your habitats to print its content.
+ * 3. End with the following phrase:
+ * -> ---
+ * -> You've reached the end of the program. To check another habitat, please restart the watcher.
+ */
+
+public class Main {
+}
