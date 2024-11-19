4f9ea3de6118a34d2b78be485d8f56389973ca50
fotistsiou <fotis.tsioumas@outlook.com>
2024-11-19 08:11:13 +0200

    Step 1

--- src/fotistsiou/java/zookeeper/step_1/Main.java
+++ src/fotistsiou/java/zookeeper/step_1/Main.java
@@ -17,4 +17,12 @@ package fotistsiou.java.zookeeper.step_1;
  */
 
 public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("I love animals!");
+        System.out.println("Let's check on the animals...");
+        System.out.println("The deer looks fine.");
+        System.out.println("The bat looks happy.");
+        System.out.println("The lion looks healthy.");
+    }
 }
