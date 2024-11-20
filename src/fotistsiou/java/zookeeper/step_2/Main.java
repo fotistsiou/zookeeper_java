d0d00aa6c621c6876b1b53b5f23a125282181019
fotistsiou <fotis.tsioumas@outlook.com>
2024-11-20 09:53:52 +0200

    Step 3

--- src/fotistsiou/java/zookeeper/step_2/Main.java
+++ src/fotistsiou/java/zookeeper/step_2/Main.java
@@ -13,7 +13,7 @@ package fotistsiou.java.zookeeper.step_2;
  */
 
 public class Main {
-    
+
     public static void main(String[] args) {
         String camel = """
             Switching on the camera in the camel habitat...
